85463c889dc0b23892c27e0807ada999b4c8f0d0
karchbsteichen <114761387+karchbsteichen@users.noreply.github.com>
2023-02-02 10:37:40 -0600

    Add files via upload
    the home work from last week

--- LineItemApp.java
+++ LineItemApp.java
@@ -1,70 +1,63 @@
+package murach.lineitem;
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class LineItemApp {
 
-	private static final String Description = null;
+    public static void main(String args[]) {
+        System.out.println("Welcome to the Line Item Calculator");
+        System.out.println();
 
-	public static void main(String args[]) {
-		
-		System.out.println("Welcome to the Line Item Calculator");
-		System.out.println();
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")) {
+            // get input from user
+            System.out.print("Enter product code: ");
+            String productCode = sc.nextLine();
 
-		// Scanner is used to read input from the System.in (console)
-		Scanner sc = new Scanner(System.in);
-		String choice = "y";
-		
-		// keep looping while the user enters y
-		while (choice.equalsIgnoreCase("y")) {
-			
-			// get input from user
-			System.out.print("Enter product code: ");
-			String productCode = sc.nextLine();
+            System.out.print("Enter quantity:     ");
+            int quantity = Integer.parseInt(sc.nextLine());
 
-			System.out.print("Enter quantity:     ");
-			int quantity = Integer.parseInt(sc.nextLine());
+            // set product price based on product code
+            double price;
+            String description;
+            if (productCode.equalsIgnoreCase("java")) {
+                price = 57.50;
+                description = "Murach's Java Programming";
+            } else if (productCode.equalsIgnoreCase("jsp")) {
+                price = 57.50;
+                description = "Murach's Java Servlets and JSP";
+            } else if (productCode.equalsIgnoreCase("mysql")) {
+                price = 54.50;
+                description = "Murach's MySQL";
+            } else if (productCode.equalsIgnoreCase("android")) {
+                price = 54.50;
+                description = "Murach's Android Programming";
+            } else {
+                description = "Unknown product";
+                price = 0;
+            }
+            
+            // calculate total
+            double total = price * quantity;
+            
+            // format and display output
+            NumberFormat currency = NumberFormat.getCurrencyInstance();
+            String message = "\nLINE ITEM\n" +
+                "Code:        " + productCode + "\n" +
+                "Description: " + description + "\n" +
+                "Price:       " + currency.format(price) + "\n" +
+                "Quantity:    " + quantity + "\n" +
+                "Total:       " + currency.format(total) + "\n";
+            System.out.println(message);
 
-			// set product price based on product code
-			double price; 
-			String desc;
-			if (productCode.equalsIgnoreCase("java")) {
-				price = 57.50;
-				desc = "door nob";
-			} else if (productCode.equalsIgnoreCase("jsp")) {
-				price = 57.50;
-				desc = "door hing";
-			} else if (productCode.equalsIgnoreCase("mysql")) {
-				price = 54.50;
-				desc = "door pin";
-			} else {
-				price = 0;
-				desc = "";
-			}
-
-			// calculate total
-			double total = price * quantity;
-
-			// format and display output
-			NumberFormat currency = NumberFormat.getCurrencyInstance();
-			String priceFormatted = currency.format(price);
-			String totalFormatted = currency.format(total);
-			String message = "\nLINE ITEM\n" + "Code:        " + productCode 
-					+ "\n" + "Price:       " + priceFormatted
-					+ "\n" + "Description: " + desc
-					+ "\n" + "Quantity:    " + quantity
-					+ "\n" + "Total:       " + totalFormatted + "\n";
-
-			// print it
-			System.out.println(message);
-
-			// see if the user wants to continue
-			System.out.print("Continue? (y/n): ");
-			choice = sc.nextLine();
-			
-			System.out.println();
-		}
-		// user didn't continue
-		sc.close();
-		System.out.println("Bye!");
-	}
+            // see if the user wants to continue
+            System.out.print("Continue? (y/n): ");
+            choice = sc.nextLine();
+            System.out.println();
+        }
+        sc.close();
+        System.out.println("Bye!");
+    }
 }
