4831c5fee59a9de830419e4627e8d1be93d20210
Jordsc <114764322+Jordsc@users.noreply.github.com>
2023-02-16 18:21:51 -0600

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,30 @@
+/**
+ * @Author jords
+ * @date Feb 10, 2023
+ * @filename Circle.java
+ */
+package murach.circle;
+
+public class Circle {
+
+	public Circle() {
+		super();
+		this.radius = 0;
+	}
+
+	double radius;
+	
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
